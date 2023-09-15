df054c51e5d5231b34af7f70c7022c5865af2594
Ed Slatt <ed.slatt@opsera.io>
2023-09-15 12:58:28 -0400

    includes t2p3_gene_expression model

---
+++ models/t2p3_gene_expression.sql
@@ -0,0 +1,23 @@
+-- Define a DBT model to calculate gene expression statistics
+WITH GeneExpressionStats AS (
+  SELECT
+    Gene_Name,
+    COUNT(*) AS Total_Samples,
+    AVG(Mutation_Position) AS Average_Mutation_Position,
+    MAX(Mutation_Position) AS Max_Mutation_Position,
+    MIN(Mutation_Position) AS Min_Mutation_Position
+  FROM
+    genomic_data
+  GROUP BY
+    Gene_Name
+)
+
+-- Select the calculated gene expression statistics
+SELECT
+  Gene_Name,
+  Total_Samples,
+  Average_Mutation_Position,
+  Max_Mutation_Position,
+  Min_Mutation_Position
+FROM
+  GeneExpressionStats
