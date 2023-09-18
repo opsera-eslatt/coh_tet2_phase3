19b01b5129fddca836a4955a6a46835cf1cec0c3
Ed Slatt <ed.slatt@opsera.io>
2023-09-18 13:55:57 -0400

    introduced token view gen max change c

--- models/t2p3_gene_expression.sql
+++ models/t2p3_gene_expression.sql
@@ -4,7 +4,7 @@ WITH GeneExpressionStats AS (
     Gene_Name,
     COUNT(*) AS Total_Samples,
     AVG(Mutation_Position) AS Average_Mutation_Position,
-    MAX(Mutation_Position) AS Max_Mutation_Position,
+    MAX(Mutation_Position) AS Max_Mutation_Position_c,
     MIN(Mutation_Position) AS Min_Mutation_Position
   FROM
     genomic_data
@@ -17,7 +17,7 @@ SELECT
   Gene_Name,
   Total_Samples,
   Average_Mutation_Position,
-  Max_Mutation_Position,
+  Max_Mutation_Position_c,
   Min_Mutation_Position
 FROM
   GeneExpressionStats
