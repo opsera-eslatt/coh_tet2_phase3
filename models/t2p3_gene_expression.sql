3b5fc26399e263e5f79bb0e575d0891178d596fb
Ed Slatt <ed.slatt@opsera.io>
2023-09-18 17:06:51 -0400

    testing to get Average_Mutation_Position_a all the way through

--- models/t2p3_gene_expression.sql
+++ models/t2p3_gene_expression.sql
@@ -3,7 +3,7 @@ WITH GeneExpressionStats AS (
   SELECT
     Gene_Name,
     COUNT(*) AS Total_Samples,
-    AVG(Mutation_Position) AS Average_Mutation_Position,
+    AVG(Mutation_Position) AS Average_Mutation_Position_a,
     MAX(Mutation_Position) AS Max_Mutation_Position,
     MIN(Mutation_Position) AS Min_Mutation_Position
   FROM
@@ -16,7 +16,7 @@ WITH GeneExpressionStats AS (
 SELECT
   Gene_Name,
   Total_Samples,
-  Average_Mutation_Position,
+  Average_Mutation_Position_a,
   Max_Mutation_Position,
   Min_Mutation_Position
 FROM
