fcb2def545478543f3dd3f9c2077a367e36b6790
Ed Slatt <ed.slatt@opsera.io>
2023-09-18 16:51:21 -0400

    upgrades opsera-dbt to support aws cli

--- opsera-dbt.sh
+++ opsera-dbt.sh
@@ -1,4 +1,6 @@
 curl ifconfig.me
 apt-get update
 apt-get install -y python3 python3-pip
-pip3 install dbt-core dbt-postgres
+pip3 install dbt-core dbt-postgres awscli
+export AWS_ACCESS_KEY_ID=${opsera-limited-s3-access-key-id}
+export AWS_SECRET_ACCESS_KEY=${opsera-limited-s3-secret-access-key}
