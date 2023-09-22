3c1e9a62e61992352365224ac140cbe3fbe0409f
opsera-eslatt <131897365+opsera-eslatt@users.noreply.github.com>
2023-09-22 10:23:20 -0400

    includes gene name

--- models/t2p3_gene_expression.sql
+++ models/t2p3_gene_expression.sql
@@ -1,6 +1,7 @@
 -- Define a DBT model to calculate gene expression statistics
 WITH GeneExpressionStats AS (
   SELECT
+    Gene_Name,
     COUNT(*) AS Total_Samples,
     AVG(Mutation_Position) AS Average_Mutation_Position,
     MAX(Mutation_Position) AS Max_Mutation_Position,
@@ -13,6 +14,7 @@ WITH GeneExpressionStats AS (
 
 -- Select the calculated gene expression statistics
 SELECT
+  Gene_Name,
   Total_Samples,
   Average_Mutation_Position,
   Max_Mutation_Position,
